18014bdeea975297ba74bf5b09d4d83e71bcddce
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-04-06 15:11:20 +0200

    dropdowm fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "./controllers";
 
 import "trix"
 import "@rails/actiontext"
+
+document.addEventListener("turbo:load", (e) => {
+  var prod_button = document.querySelector('#prod-dropdown');
+  var prod_dropdown = document.querySelector('#prod-body');
+
+  prod_button.addEventListener('click', e => {
+    console.log(prod_dropdown.classList)
+    prod_dropdown.classList.toggle('hidden');
+  })
+})
