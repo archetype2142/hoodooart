8a6e506794be441e123faa42c1276814f4d0ccd6
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-04-06 12:47:38 +0200

    added contact form

--- app/components/quantity_input_component.rb
+++ app/components/quantity_input_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class QuantityInputComponent < ViewComponent::Base
-  def initialize(quantity:)
+  def initialize(quantity:, name:)
     @quantity = quantity
+    @name = name
   end
 end
