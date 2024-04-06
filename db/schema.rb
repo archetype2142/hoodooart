41793a7ab5a33cb3b6e763c463ab75c3304a518b
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-04-06 02:23:28 +0200

    Added cart features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_16_163145) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_05_231630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_16_163145) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "description"
+    t.string "sku"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
