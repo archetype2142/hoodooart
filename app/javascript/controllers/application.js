4421ffd531dd442e25d5c6b9061c543b1f037b71
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-04-06 15:13:16 +0200

    dropdowm fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,13 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.addEventListener("turbo:load", (e) => {
+  var prod_button = document.querySelector('#prod-dropdown');
+  var prod_dropdown = document.querySelector('#prod-body');
+
+  prod_button.addEventListener('click', e => {
+    console.log(prod_dropdown.classList)
+    prod_dropdown.classList.toggle('hidden');
+  })
+});
