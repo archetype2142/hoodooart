d1d7511485492775d84d157b08999bfec81bf289
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-05-08 01:58:09 +0200

    random

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "./controllers";
 
 import "trix"
 import "@rails/actiontext"
+
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
+import 'photoswipe/dist/photoswipe.css'
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#gallery--getting-started',
+  children: 'a',
+  pswpModule: () => PhotoSwipe
+});
+
+lightbox.init();
